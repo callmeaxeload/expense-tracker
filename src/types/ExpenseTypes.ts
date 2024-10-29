43beb408669aa20285f2c3c849fae2de8cf7d2f4
Naufal Waliudin Falah <naufaljoe469@gmail.com>
2024-10-29 08:50:04 +0700

    adding ExpenseTypes

---
+++ src/types/ExpenseTypes.ts
@@ -0,0 +1,9 @@
+export type Expense = {
+  id: string,
+  amount: number,
+  date: string,
+  category: string
+  description: string,
+}
+
+export type Category = 'Food' | 'Transportation' | 'Groceries' | 'Entertainment' | 'Other'
